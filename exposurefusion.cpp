25a16c055053a84002fa1555b46a27d75b628eb4
paulcaron <paul.caron@polytechnique.edu>
2018-11-07 10:21:21 +0100

    Update exposurefusion.cpp

--- exposurefusion.cpp
+++ exposurefusion.cpp
@@ -2,6 +2,12 @@
 #include <iostream>
 #include <fstream>
 #include "image.h"
+
 int main(int argc, char** argv){
+	Mat I = imread("../road.jpg");
+	cout << I.at<Vec3b>(2,2);
+	Mat Ihsv;
+	cvtColor(I, Ihsv, CV_BGR2HSV);
+	cout << Ihsv.at<Vec3b>(2, 2);
     return 0;
 }
